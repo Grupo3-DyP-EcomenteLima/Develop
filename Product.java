37b9c02861adf564c9b7eacde16902b0cb9bdd77
EricCuevas <81304187+Cire1302@users.noreply.github.com>
2022-10-30 21:19:04 -0500

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,28 @@
+public class Product {
+    private String brand;
+    private String p_name;
+    private float price;
+    private int quantity;
+    private String desc;
+    private int stock;
+
+    public float getPrice() {return price;}
+    public int getQuantity() {return quantity;}
+    public int getStock() {return stock;}
+    public String getBrand() {return brand;}
+    public String getDesc() {return desc;}
+    public String getp_name() {return p_name;}
+
+    public void setDesc(String desc) {this.desc = desc;}
+    public void setPrice(float price) {this.price = price;}
+    public void setStock(int stock) {this.stock = stock;}
+    public void setp_name(String p_name) {this.p_name = p_name;}
+
+    public void Print_product(){
+
+    }
+
+    public void set_product(){
+
+    }
+}
