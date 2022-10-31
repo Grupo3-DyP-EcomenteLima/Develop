0486cc799ad8d1d56682499ac37268998f2a7ce1
EricCuevas <81304187+Cire1302@users.noreply.github.com>
2022-10-30 21:33:17 -0500

    Add files via upload

--- Product.java
+++ Product.java
@@ -1,12 +1,16 @@
 public class Product {
     private String brand;
     private String p_name;
-    private float price;
+    private double price;
     private int quantity;
     private String desc;
     private int stock;
 
-    public float getPrice() {return price;}
+    //Product(){ String p_name = ""; String brand = ""; double price = 0.0; ... ){
+
+   // }
+
+    public double getPrice() {return price;}
     public int getQuantity() {return quantity;}
     public int getStock() {return stock;}
     public String getBrand() {return brand;}
@@ -19,12 +23,16 @@ public class Product {
     public void setp_name(String p_name) {this.p_name = p_name;}
 
     public void Print_product(){
-
+        System.out.println(p_name);
+        System.out.println("Marca: "+brand);
+        System.out.println("Stock: "+stock);
+        System.out.println(desc);
+        System.out.println("Cantidad por compra: "+quantity+" Unidades");
+        System.out.println("Precio: $"+price);
+        System.out.println(" ");
     }
 
     public void set_product(){
 
     }
-    
-    //en un rato añado los metodos
 }
