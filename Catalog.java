39767cc70d32af5ed71db58ab7cffb3d1db9d8e4
EricCuevas <81304187+Cire1302@users.noreply.github.com>
2022-10-30 21:32:49 -0500

    Add files via upload

--- Catalog.java
+++ Catalog.java
@@ -5,26 +5,19 @@ public class Catalog {
     public void printcatalog(){
 
         for(int i = 0; i <catalogo.length; i++){
-
-            System.out.println(catalogo[i].getp_name());
-            System.out.println("Marca: "+catalogo[i].getBrand());
-            System.out.println("Stock: "+catalogo[i].getStock());
-            System.out.println(catalogo[i].getDesc());
-            System.out.println("Cantidad por compra: "+catalogo[i].getQuantity()+" Unidades");
-            System.out.println("Precio: $"+catalogo[i].getPrice());
-            System.out.println(" ");
-
+            catalogo[i].Print_product();
         }
 
     }
 
     public Product getProducto(int n){
-
+        Product temp = new Product();
         for(int i = 0; i <catalogo.length; i++){
 
             if(n == i+1){ return catalogo[i];}
 
         }
+
+        //return
     }
-    //el print deberia estar bien pero no estoy seguro del getProducto()
 }
