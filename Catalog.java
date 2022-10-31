7af28b7cc7ea9b88ede62e4d4cc15a2f6b716cd8
EricCuevas <81304187+Cire1302@users.noreply.github.com>
2022-10-30 21:20:22 -0500

    Add files via upload

---
+++ Catalog.java
@@ -0,0 +1,29 @@
+public class Catalog {
+
+    private Product catalogo[] = new Product[3];
+
+    public void printcatalog(){
+
+        for(int i = 0; i <catalogo.length; i++){
+
+            System.out.println(catalogo[i].getp_name());
+            System.out.println("Marca: "+catalogo[i].getBrand());
+            System.out.println("Stock: "+catalogo[i].getStock());
+            System.out.println(catalogo[i].getDesc());
+            System.out.println("Cantidad por compra: "+catalogo[i].getQuantity()+" Unidades");
+            System.out.println("Precio: $"+catalogo[i].getPrice());
+            System.out.println(" ");
+
+        }
+
+    }
+
+    public Product getProducto(int n){
+
+        for(int i = 0; i <catalogo.length; i++){
+
+            if(n == i+1){ return catalogo[i];}
+
+        }
+    }
+}
