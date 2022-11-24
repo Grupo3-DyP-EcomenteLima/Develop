a315575f7096ac88242fbecd04ccc0d4009c4b2a
Pier0Mendoz4 <104656858+Pier0Mendoz4@users.noreply.github.com>
2022-11-24 12:40:36 -0500

    Add files via upload

--- UsuarioBuilder/src/main/java/pe/edu/upc/UsuarioBuilder/Main.java
+++ UsuarioBuilder/src/main/java/pe/edu/upc/UsuarioBuilder/Main.java
@@ -1,21 +1,50 @@
 package pe.edu.upc.UsuarioBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    static Scanner op = new Scanner(System.in);
+    static Scanner correo = new Scanner(System.in);
+    static Scanner contrasena = new Scanner(System.in);
 
-        System.out.println("**Prueba user builder**");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        List<User> usuarios = new ArrayList<>();
+        int opcion=0;
+        do{
+            System.out.println("**Prueba user builder**");
+            System.out.println("1. Registrar cuenta vendedor");
+            System.out.println("2. Mostrar vendedores registrados");
+            System.out.println("3. Salir");
+            opcion = op.nextInt();
+            switch (opcion) {
+                case 1:
+                    UserDirector director = new UserDirector();
+                    UserBuilder seller = new SellerConcreteBuilder();
+                    director.construct(seller);
+                    User u1 = seller.getUser();
+                    usuarios.add(u1);
+                    break;
+                case 2:
 
-        UserDirector director = new UserDirector();
+                    System.out.println("Ingresa tu correo registrado: ");
+                    String co = correo.nextLine();
+                    System.out.println("Ingresa tu contrasena: ");
+                    String con = contrasena.nextLine();
 
-        SellerBuilder seller = new SellerConcreteBuilder();
+                    for(User obj: usuarios){
+                        System.out.println("***********");
+                       obj.mostrardatos();
+                    }
 
-        List<SellerConcreteBuilder> listaSeller;
 
-        director.construct(seller);
-        User u1 = seller.getUser();
+                    break;
+            }
 
+
+        }while (opcion !=3);
 
 
     }
--- UsuarioBuilder/src/main/java/pe/edu/upc/UsuarioBuilder/User.java
+++ UsuarioBuilder/src/main/java/pe/edu/upc/UsuarioBuilder/User.java
@@ -1,20 +1,18 @@
 package pe.edu.upc.UsuarioBuilder;
 
-
-//1
 public class User {
-
     private String fullName;
-    private String email;
-    private String password;
-
+    public String email;
+    public String password;
+    private long bankAccount;
+    private String brand;
     public User(){
-
         fullName = null;
         email = null;
         password = null;
+        bankAccount = 0;
+        brand = null;
     }
-
     public void setFullName(String fullName){
         this.fullName = fullName;
     }
@@ -24,7 +22,12 @@ public class User {
     public void setPassword(String password){
         this.password = password;
     }
-
+    public void setBankAccount(long bankAccount){
+        this.bankAccount = bankAccount;
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
     public String getFullName(){
         return this.fullName;
     }
@@ -33,5 +36,27 @@ public class User {
     }
     public String getPassword(){
         return this.password;
+    }
+    public long getBankAccount(){
+       return bankAccount;
+    }
+    public String getBrand(){
+        return brand;
+    }
+
+    public void addProduct() {
+        System.out.println("Producto agregado");
+    }
+    public void deleteProduct() {
+        System.out.println("Producto eliminado");
+    }
+    public void modProduct() {
+        System.out.println("Producto modificado");
+    }
+    public void mostrardatos(){
+        System.out.println("Nombre: "+fullName);
+        System.out.println("Marca: "+brand);
+        System.out.println("Correo: "+email);
+        System.out.println("Cuenta bancaria: "+bankAccount);
     }
 }
